3546621d0881c60cfafe9bf65acf45bb81bc91c1
rochan <rochan123@gmail.com>
2026-05-31 11:53:37 +0530

    added by sub2

--- 02.py
+++ 02.py
@@ -1,6 +1,9 @@
 a=int(input("enter a value:"))
 b=int(input("enter b value:"))
-if(a>b):
+c=int(input("enter c value:"))
+if(a>b) and (a>c):
     print("A is greater")
-else:
+if(b>a) and (b>c):
     print("B is greater")
+else:
+    print("C is greater")
